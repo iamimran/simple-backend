cd53c142aaeae32e59b61acedd31bb2ba7f3f678
Imran Ahmed <iam.imran@yahoo.com>
2023-12-18 18:48:59 +0500

    1. Creates ApiError class 2. creates ApiResponse class for error and response standardisation

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => {
+  (req, resp, next) => {
+    Promise.resolve(requestHandler(req, resp, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+// const asyncHandler = (func) => async (req, resp, next) => {
+//   try {
+//     await func(req, resp, next);
+//   } catch (error) {
+//     resp.status(err.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
